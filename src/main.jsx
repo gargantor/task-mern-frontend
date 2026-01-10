ebc82ccc3e603122a2001b832760543bfaf9ab32
Hanif Robbani <hanif03@gmail.com>
2026-01-10 11:43:30 +0700

    06.01

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store} from './App/store';
+import App from "./App";
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+    <>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </>
+
 )
