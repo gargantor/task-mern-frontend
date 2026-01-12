a5b4fe7829891ff947c85947a249826c334d62ef
Hanif Robbani <hanif03@gmail.com>
2026-01-12 12:49:09 +0700

    09.01 task form

--- src/components/Dasboard.jsx
+++ src/components/Dasboard.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
+import TaskForm from './TaskForm';
 const Dashboard = () => {
+    const navigate = useNavigate();
+    const { user } = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if(!user) navigate('/login');
+    }, [user, navigate])
     return (
-        <div>Dashboard</div>
+        <>
+            <section className='heading'>
+                <h1>Welcome {user && user.name}</h1>
+                <p>Tasks Dashboard</p>
+            </section>
+            <TaskForm />
+        </>
     )
 }
 
