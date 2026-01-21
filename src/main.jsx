42cb15ef810b1b43c990340dc1ec287713fce4ae
Hanif Robbani <hanif03@gmail.com>
2026-01-21 15:18:03 +0700

    backend integrated 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store} from './App/store';
-import App from "./App";
+import { store} from './app/store';
+import App from "./app";
 import './index.css';
 import axios from "axios";
 
